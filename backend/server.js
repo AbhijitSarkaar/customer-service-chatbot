941874874ccc24ef7653438d07247978546ca811
AbhijitSarkaar <abhijit.sarkaar26@gmail.com>
2024-11-05 15:40:49 +0530

    add console

--- backend/server.js
+++ backend/server.js
@@ -8,4 +8,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/", indexRoutes);
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+  console.log("listening on", process.env.PORT);
+});
